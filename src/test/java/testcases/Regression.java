ab7b41b5e599403dffab982659803624cd5c59fc
OraniumTech <oraniumtech@gmail.com>
2023-05-12 13:33:04 +0530

    After merge conflicts

--- src/test/java/testcases/Regression.java
+++ src/test/java/testcases/Regression.java
@@ -2,7 +2,9 @@ package testcases;
 
 public class Regression {
 	public static void main(String[] args) {
-		System.out.println("regression chanes done- Remote");
+
+		System.out.println("regression chanes done- Local ");
+
 	}
 
 }
